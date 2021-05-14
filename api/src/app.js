f4d87a305cb0d9cdc54227838e19794efc892dbb
Carlangas11 <73184352+Carlangas11@users.noreply.github.com>
2021-05-13 23:43:43 -0400

    home with filters

--- api/src/app.js
+++ api/src/app.js
@@ -9,7 +9,9 @@ require("./db");
 const server = express();
 
 server.name = "API";
-
+server.set("view engine", "ejs");
+server.set("views", __dirname + "/views");
+server.use(express.static('./src/public'));
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
@@ -18,7 +20,7 @@ server.use("/", routes);
 
 
 server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
+    // res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header(
       'Access-Control-Allow-Headers',
@@ -31,7 +33,7 @@ server.use((err, req, res, next) => {
   const status = err.status || 500;
   const message = err.message || err;
   console.error(err);
-  res.status(status).send(message);
+  res.status(status).render("404", {title: "404", description:message});
 });
 
 module.exports = server;
--- api/src/routes/product/product.js
+++ api/src/routes/product/product.js
@@ -1,20 +1,38 @@
-const express = require("express")
-const router = express.Router()
-const {Product, Category} = require('../../db')
+const express = require("express");
+const router = express.Router();
+const { Product, Category } = require("../../db");
+// const ejs = require('ejs');
+router.use(express.json());
 
-// const{ Product }= require('../../models/Product')
+router.get("/", async (req, res) => {
+  const filter = req.query.name;
+  const categories = await Category.findAll();
+  console.log(filter);
+  if (!filter) {
+    const products = await Product.findAll({
+      include: { model: Category, as: "category_info" },
+    });
+    
+    // console.log(categories.map(c => c.dataValues))
+    res.render("index", { data: products, categories: categories });
+  } else {
+    const products = await Product.findAll({
+      include: { model: Category,where:{name: filter},  as: "category_info" },
+    });
+    res.render("index", { data: products, categories: categories });
+  }
+});
 
-router.use(express.json())
 
-router.get("/", async (req,res)=>{
-    // const product = await Product.findAll()
-    // console.log(product)
-    const product = await Product.findAll({include: { model: Category, as: "category_info"}})
 
-    res.json(product)
-})
-router.get("/product", (req,res)=>{
-    res.json("Devolver productos")
-})
 
-module.exports = router
+router.get("/product", (req, res) => {
+  res.json("Devolver productos");
+});
+
+router.use((err, req, res, next) => {
+  const message = err.message || err;
+  console.error(err);
+  res.status(404).render("404", { title: "404", description: message });
+});
+module.exports = router;
