f6aa4646f43d435ecfafc895f6b0c97f1e107899
Carlangas11 <73184352+Carlangas11@users.noreply.github.com>
2021-05-14 10:53:38 -0400

    commit front

--- api/src/routes/product/product.js
+++ api/src/routes/product/product.js
@@ -6,16 +6,18 @@ router.use(express.json());
 
 router.get("/", async (req, res) => {
   const filter = req.query.name;
+  const search = req.query.search;
   const categories = await Category.findAll();
   console.log(filter);
   if (!filter) {
     const products = await Product.findAll({
       include: { model: Category, as: "category_info" },
     });
-    
+
+    // console.log(products.map(c => c.dataValues))
     // console.log(categories.map(c => c.dataValues))
     res.render("index", { data: products, categories: categories });
-  } else {
+  } else if(!search){
     const products = await Product.findAll({
       include: { model: Category,where:{name: filter},  as: "category_info" },
     });
@@ -26,13 +28,17 @@ router.get("/", async (req, res) => {
 
 
 
-router.get("/product", (req, res) => {
-  res.json("Devolver productos");
+router.get("/orderDetail", async(req, res) => {
+  const categories = await Category.findAll();
+  res.render("order",{ categories: categories });
 });
 
-router.use((err, req, res, next) => {
+
+
+router.use(async(err, req, res, next) => {
+  const categories = await Category.findAll();
   const message = err.message || err;
   console.error(err);
-  res.status(404).render("404", { title: "404", description: message });
+  res.status(404).render("404", { title: "404", description: message,categories: categories });
 });
 module.exports = router;
